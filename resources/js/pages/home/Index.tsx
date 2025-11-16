fd1a2826e2a001062f191d5b8b13fc0d7a87e6d9
Alco2 <abdou.lahboub.wwe@gmail.com>
2025-11-16 22:53:16 +0100

    fix font issue

--- resources/js/pages/home/Index.tsx
+++ resources/js/pages/home/Index.tsx
@@ -1,13 +1,13 @@
 import NavbarLayout from "@/layouts/navbar-layout";
 import ServiceIntro from "./ServiceIntro";
-import { Box, Check, ShoppingBasket, ShoppingCart } from "lucide-react";
+import { Box, Check, ShoppingCart } from "lucide-react";
 
 
 
 export default function Index() {
     return (
         <NavbarLayout>
-            <div className="flex flex-wrap items-center justify-center px-4">
+            <div className="flex flex-wrap items-center justify-center px-4 my-4">
                 <div className="sm:w-1/2 space-y-10">
                     <div>
                         <h1 className="font-Italiana font-bold text-3xl sm:text-5xl max-xs:text-xl text-black">Welcome to Elixir Shop <br/> online store</h1>
@@ -23,17 +23,23 @@ export default function Index() {
                         </div>
                     </div>
                     <div>
-                        <button className="text-white py-2 px-4 bg-black rounded-sm">Shop Now</button>
+                        <button className="text-white py-2 px-4 bg-black rounded-md">Shop Now</button>
                     </div>
                 </div>
                 <div className="max-sm:hidden">
                     <img src="/assets/intro.webp" width={450} alt="" />
                 </div>
             </div>
-            <div className="flex justify-center px-20">
+            <div className="flex justify-center px-20 bg-black text-white py-10">
                 <ServiceIntro icon={<Check className="size-10" />} title='Original Authentie' description='it is our policy to provide original authentic fragrances to our costumers and community' />
                 <ServiceIntro icon={<Box className="size-10" />} title='Brand New Product' description='We will not provide any second fragrances all our fragrances are sent directly from the companies' />
                 <ServiceIntro icon={<ShoppingCart className="size-10" />} title='Fast Shipping' description='You have the right to use our fragrances as fast as possible and we will make sure you have your right' />
+            </div>
+             <div className="text-center py-8">
+                <h1 className="text-5xl font-italiana font-bold max-sm:text-4xl max-xs:text-3xl">Recommendation</h1>
+                <div className="flex justify-center text-center">
+                    <p className="font-Opensans w-80">These fragrances are the best selling ones. it is designed precisly by the designers.</p>
+                </div>
             </div>
         </NavbarLayout>
     )
--- resources/js/pages/home/ServiceIntro.tsx
+++ resources/js/pages/home/ServiceIntro.tsx
@@ -6,7 +6,7 @@ const ServiceIntro = ({icon, title, description}:{icon:ReactNode, title:string, 
     <div className="">
         {icon}
         <div className="ml-4 mt-2">
-            <h5 className="mb-1 text-2xl font-bold font-Italiana">{title}</h5>
+            <h5 className="mb-1 text-2xl font-bold font-italiana">{title}</h5>
             <p className="text-sm opacity-70 font-semibold font-Opensans">{description}</p>
         </div>
     </div>
