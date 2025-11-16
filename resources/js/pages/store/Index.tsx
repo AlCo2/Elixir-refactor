4be2dea9fad46e30492693277895d5f71b527818
Alco2 <abdou.lahboub.wwe@gmail.com>
2025-11-16 20:05:48 +0100

    setup first part in landing page

--- resources/js/pages/store/Index.tsx
+++ resources/js/pages/store/Index.tsx
@@ -5,7 +5,29 @@ import NavbarLayout from "@/layouts/navbar-layout";
 export default function Index() {
     return (
         <NavbarLayout>
-            test
+            <div className="flex flex-wrap items-center justify-center px-4">
+                <div className="sm:w-1/2 space-y-10">
+                    <div>
+                        <h1 className="font-Italiana font-bold text-3xl sm:text-5xl max-xs:text-xl text-black">Welcome to Elixir Shop <br/> online store</h1>
+                    </div>
+                    <div className="flex gap-10">
+                        <div>
+                            <p className="font-Poppins font-bold mb-1">220+</p>
+                            <p className="font-Poppins opacity-60 font-semibold text-sm">Total Products</p>
+                        </div>
+                        <div>
+                            <p className="font-Poppins font-bold mb-1">2+</p>
+                            <p className="font-Poppins opacity-60 font-semibold text-sm">Years on the market</p>
+                        </div>
+                    </div>
+                    <div>
+                        <button className="text-white py-2 px-4 bg-black rounded-sm">Shop Now</button>
+                    </div>
+                </div>
+                <div className="max-sm:hidden">
+                    <img src="/assets/intro.webp" width={450} alt="" />
+                </div>
+            </div>
         </NavbarLayout>
     )
 }
